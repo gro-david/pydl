c866bfb3911a14702741d03ccc58541d7006ba92
gro-david <99834433+gro-david@users.noreply.github.com>
2023-09-15 19:55:36 +0200

    Fixed notification sound path error

--- pydl-linux/pydl/libraries/notifier.py
+++ pydl-linux/pydl/libraries/notifier.py
@@ -10,7 +10,7 @@ notification = Notify(default_notification_application_name="pydl")
 
 
 def finished(title):
-    notification.audio = f"{get_app_path.main()}/sounds/notif-sound.wav"
+    os.path.join(get_app_path.main(), os.path.join("sounds", "notif-sound.wav"))
     notification.title = "pydl - Finished"
     notification.message = f'pydl has finished downloading "{title}"!'
     notification.send()
@@ -18,7 +18,7 @@ def finished(title):
 
 
 def error(title):
-    notification.audio = f"{get_app_path.main()}/sounds/error-sound.wav"
+    os.path.join(get_app_path.main(), os.path.join("sounds", "error-sound.wav"))
     notification.title = "pydl - Error"
     notification.message = f'pydl has encountered an error while downloading "{title}"!'
     notification.send()
